25946b8c116051934baa13bcd5ac1cb68895c5f4
B.N <19542357+BNSC1@users.noreply.github.com>
2022-07-15 08:55:19 +0800

    - Add: attraction info list to attraction info list page.

--- app/src/main/java/com/bn/taipeitravelinfo/ui/AttractionInfoListFragment.kt
+++ app/src/main/java/com/bn/taipeitravelinfo/ui/AttractionInfoListFragment.kt
@@ -3,7 +3,10 @@ package com.bn.taipeitravelinfo.ui
 import android.os.Bundle
 import android.view.View
 import com.bn.taipeitravelinfo.arch.ObserveStateFragment
+import com.bn.taipeitravelinfo.arch.OnItemClickListener
+import com.bn.taipeitravelinfo.data.model.Attraction
 import com.bn.taipeitravelinfo.databinding.FragmentAttractionInfoListBinding
+import com.bn.taipeitravelinfo.ui.adapter.AttractionInfoListAdapter
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -16,7 +19,15 @@ class AttractionInfoListFragment : ObserveStateFragment<FragmentAttractionInfoLi
         super.onViewCreated(view, savedInstanceState)
 
         with(binding) {
+            attractionList.adapter = setupAttractionInfoListAdapter()
 
         }
     }
+
+    private fun setupAttractionInfoListAdapter() =
+        AttractionInfoListAdapter(object : OnItemClickListener<Attraction> {
+            override fun onItemClick(item: Attraction) {
+
+            }
+        })
 }
