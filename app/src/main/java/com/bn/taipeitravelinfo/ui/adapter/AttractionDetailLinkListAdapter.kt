4bbd420b1ec0583d3f90630cc8e93e83afe0d820
B.N <19542357+BNSC1@users.noreply.github.com>
2022-07-19 10:01:57 +0800

    - Add: attraction detail link list adapter

---
+++ app/src/main/java/com/bn/taipeitravelinfo/ui/adapter/AttractionDetailLinkListAdapter.kt
@@ -0,0 +1,18 @@
+package com.bn.taipeitravelinfo.ui.adapter
+
+import com.bn.taipeitravelinfo.arch.BaseListAdapter
+import com.bn.taipeitravelinfo.arch.OnItemClickListener
+import com.bn.taipeitravelinfo.databinding.ItemAttractionDetailLinkBinding
+
+data class DetailLink(val title: String, val url: String)
+
+class AttractionDetailLinkListAdapter(private val clickListener: OnItemClickListener<DetailLink>) :
+    BaseListAdapter<ItemAttractionDetailLinkBinding, DetailLink>() {
+    override val items = mutableListOf<DetailLink>()
+    override val bindAction = { binding: ItemAttractionDetailLinkBinding, item: DetailLink ->
+        with(binding.linkBtn) {
+            text = item.title
+            setOnClickListener { clickListener.onItemClick(item) }
+        }
+    }
+}
